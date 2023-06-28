b35d9ae7bc84b29a71615a809e142a4c94bcc04f
raissa campos <raissa.campos26@gmail.com>
2023-06-28 14:52:01 +0100

    feat: inital structure header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,31 @@
+import { NavLink } from "react-router-dom";
+import { Dog } from "phosphor-react";
+
+import styles from "./index.module.css";
+
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <NavLink to="/" className={styles.mainTitle}>
+        <Dog size={32} color="#cdb4db" />
+        <h1 className={styles.title}>Doggy Gallery</h1>
+      </NavLink>
+      <nav>
+        <ul className={styles.menuContainer}>
+          <li className={styles.menuItem}>
+            <NavLink to="/">Menu 1</NavLink>
+          </li>
+          <li className={styles.menuItem}>
+            <NavLink to="/">Menu 2</NavLink>
+          </li>
+          <li className={styles.menuItem}>
+            <NavLink to="/">Menu 4</NavLink>
+          </li>
+          <li className={styles.menuItem}>
+            <NavLink to="/">Menu 4</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
